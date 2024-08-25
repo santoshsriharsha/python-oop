68183b5d4d2e32e4540c3f774138391c6d17d009
Harsha <40897247+santoshsriharsha@users.noreply.github.com>
2024-08-25 14:31:50 +0530

    Modify Plant class to inherit from Tree class

--- 02-Inheritance.py
+++ 02-Inheritance.py
@@ -7,15 +7,10 @@ class Tree(object):
         print('This is a {} tree and it is {} years old'.format(self.name, self.age))
 
 
-class Plant(object):
+class Plant(Tree):
     def __init__(self, name, age, location):
-        self.name = name
-        self.age = age
+        super().__init__(name, age)
         self.location = location
-
-    def details(self):
-        print('This is a {} plant, it is {} years old and it is located at {}'
-              .format(self.name, self.age, self.location))
 
 
 t = Tree('Neem', 100)
