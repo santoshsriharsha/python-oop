1fe8404d8e5373ac56404d3522f5f8181344fe16
Harsha <40897247+santoshsriharsha@users.noreply.github.com>
2024-08-25 16:20:40 +0530

    Add more overridden built in methods

--- 03-Overriding Methods.py
+++ 03-Overriding Methods.py
@@ -19,13 +19,29 @@ class Point(object):
     def __str__(self):
         return '({}, {})'.format(self.x, self.y)
 
+    def __len__(self):
+        import math
+        return int(math.sqrt(self.x**2 + self.y**2))
+
+    def __gt__(self, p):
+        return len(self) > len(p)
+
+    def __ge__(self, p):
+        return len(self) >= len(p)
+
+    def __lt__(self, p):
+        return len(self) < len(p)
+
+    def __le__(self, p):
+        return len(self) <= len(p)
+
+    def __eq__(self, p):
+        return self.x == p.x and self.y == p.y
+
 
 p1 = Point(4,3)
 p2 = Point(7, 10)
 p3 = Point(2, 6)
 p4 = Point(3, 8)
-p5 = p1 + p2
-p6 = p3 - p4
-p7 = p1 * p4
-
-print(p5, p6, p7)
+print(len(p1))
+print(p1 > p4, p1 <= p3, p1 == p1)
